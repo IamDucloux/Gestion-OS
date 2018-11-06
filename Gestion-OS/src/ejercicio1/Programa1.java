ffd45786fefc50fb500afc6dc2e8450352cbb69b
iamducloux <T10@PC283>
2018-11-05 21:36:47 -0600

    Ejercicio 1

---
+++ Gestion-OS/src/ejercicio1/Programa1.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author T10
+ */
+public class Programa1 {
+    private String nombre;
+    private String escuela;
+    private String especialidad;
+    private String semestre;
+
+    public Programa1(String nombre, String escuela, String especialidad, String semestre) {
+        this.nombre = nombre;
+        this.escuela = escuela;
+        this.especialidad = especialidad;
+        this.semestre = semestre;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEscuela() {
+        return escuela;
+    }
+
+    public void setEscuela(String escuela) {
+        this.escuela = escuela;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+    
+    
+    
+    
+        
+
+   
+}
